5e8a67c2e692588069ea0d56678829a827524ee1
Mansi agarwal <mansiagarwal293@gmail.com>
2025-02-14 14:17:25 +0530

    all package install

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App.jsx";
-
+import { Provider as ChakraProvider } from "./components/ui/provider.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </StrictMode>
 );
